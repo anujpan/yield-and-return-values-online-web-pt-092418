c85b943b998b3042062b5fc7d50cf8fe8e3f2ca3
Anuj Panchal <anujpan1@gmail.com>
2018-10-11 18:14:13 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -5,18 +5,11 @@ def hello(array)
   collection = []
 
   while i < array.length
-    # collection << yield(array[i])
-
-    shit(i)
+    collection << yield(array[i])
     i += 1
   end
   
   collection
 end
-
-def shit(name)
-  puts "Hi, #{name}"
-end
-
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
